093ab839708aeaee24ab2dd718beed3803559280
Saphielle Akiyama <saphielle.akiyama@gmail.com>
2022-04-24 01:17:10 +0200

    Pas loin le chemin n'est parfois pas change

--- chemins_auto_evitants/cae.py
+++ chemins_auto_evitants/cae.py
@@ -3,7 +3,9 @@ Issu de la partie III du sujet Mines-Pont 2021 d'informatique commune
 """
 from random import randrange
 
-Point = tuple[int, int]
+Vecteur = tuple[int, ...]
+Vecteur3D = tuple[float, float, float]
+Point = Vecteur2D = tuple[int, int]
 Chemin = list[Point]
 Matrice = list[list[int]]
 
@@ -36,7 +38,7 @@ def genere_chemin_naif(n: int) -> Chemin | None:
         i = randrange(len(possibles))
         position_actuelle = possibles[i]
 
-        atteints.append(position_actuelle)
+        atteints.append(position_actuelle)  # type: ignore
 
     return atteints
 
@@ -81,7 +83,7 @@ def img_mat(mat: Matrice, p: Point) -> Point:
     return (a * x + b * y, c * x + d * y)
     
 
-def sub(p1: Point, p2: Point) -> Point:
+def sub(p1: Vecteur2D, p2: Vecteur2D) -> Vecteur2D:
     """
     Soustraction de deux points
     Dans la base canonique, cela revient a considerer les coordonnees de p1 lorsque l'origine 
@@ -93,7 +95,7 @@ def sub(p1: Point, p2: Point) -> Point:
     return (x1 - x2, y1 - y2)
 
 
-def add(p1: Point, p2: Point) -> Point:
+def add(p1: Vecteur2D, p2: Vecteur2D) -> Vecteur2D:
     """
     Addition de deux points
     Dans la base canonique, cela revient a considerer les coordonnees de p1 lorsque l'origine 
@@ -132,49 +134,76 @@ def rotation(chemin: Chemin, i_piv: int, a: int) -> Chemin:
     return nv_chemin
 
 
-def ajustement(os: Point, op: Point) -> tuple[Point, Point]:
+def produit_vectoriel(v1: Vecteur3D, v2: Vecteur3D) -> Vecteur3D:
     """
-    Le vecteur op est le vecteur pivot->precedent
-    Le vecteur os est le vecteur pivot->suivant
-
-    On les fait tourner afin de se retrouver dans l'une des situations suivantes 
-
-    ------------------------------------------------------
-    | Configuration A | Configuration B | Configuration C |
-    |                 |                 |                 |
-    | S               | O -> P          | O -> P          |
-    | |               |   -> S          | |               |
-    | O -> P          |                 | S               |
-    -------------------------------------------------------
+    Produit vectoriel entre deux vecteurs
     """
-    rot = mat_rot(1)
-    while op[1] != 0 and op[1] < 0:
-        op = img_mat(rot, op) 
-        os = img_mat(rot, os)
+    a, b, c = v1 
+    d, e, f = v2
 
-    return os, op
+    return (
+        b * f - c * e,
+        c * d - a * f, 
+        a * e - b * d
+    )
 
 
-def tire_a(os: Point) -> int:
+def angle_exclu(op: Vecteur2D, os: Vecteur2D) -> int:
     """
-    Apres ajustement, tire un angle qui n'est pas possible au vu des points precedents
-    et suivants
-    """ 
-    # Configuration A, pas de rotations d'angle pi/2
-    x, y = os
-    if y > 0:
-        return 2 * randrange(2)
+    Trouve l'angle qui doit etre exclu en utilisant la definition du produit 
+    vectoriel de deux vecteurs unitaires
 
-    # Configuration B, pas de rotations d'angle pi
-    elif y < 0:
-        return randrange(2)
+    os ^ op = sin(os, op) * u
 
-    # Configuration C, pas de rotation d'angle pi
+    On a trois configurations possibles:
+    
+    -------
+    S
+    | 
+    O -> P 
+
+    Leur produit vectoriel donne un vecteur selon +Uz
+    ------
+
+    O -> P 
+      -> S 
+
+    Leur produit vectoriel est nul
+    ------
+
+    O -> P 
+    |
+    S
+
+    Dont le produit vectoriel est selon -Uz
+    """
+    xs, ys = os 
+    os3 = (xs, ys, 0)
+
+    xp, yp = op 
+    op3 = (xp, yp, 0)
+
+    _, _, z = produit_vectoriel(os3, op3)
+
+    if z == 0:
+        return 0
+
+    elif z == 1:
+        return 1
+    
     else:
-        return 1 + randrange(2)
+        return 2
 
 
-def genere_a(chemin: Chemin, i_piv: int) -> int:
+def tire_a(angles_exclus: list[int]) -> int:
+    """
+    Tire un angle parmi ceux qui sont encore utilisables
+    """
+    possibles = [x for x in range(3) if x not in angles_exclus]
+    return possibles[randrange(len(possibles))]
+
+
+def genere_a(chemin: Chemin, i_piv: int, angles_exclus: list) -> int:
     """
     Selectionne aleatoirement un type de rotation en evitant celles qui sont a premiere vue 
     impossibles en observant les points alentours
@@ -188,14 +217,15 @@ def genere_a(chemin: Chemin, i_piv: int) -> int:
     precedent = chemin[i_piv - 1]
     suivant = chemin[i_piv + 1]
 
-    # On considere les vecteurs pivot->precedent et pivot->suivant
+    # On cherche a determiner l'angle entre les vecteurs 
+    # pivot->precedent et pivot->suivant
     op = sub(precedent, pivot) 
     os = sub(suivant, pivot)
-
-    os, op = ajustement(os, op)       
         
-    return tire_a(os)
+    exclu = angle_exclu(op, os)
+    angles_exclus.append(exclu)
 
+    return tire_a(angles_exclus)
 
 def initialisation(n: int):
     """
@@ -204,26 +234,39 @@ def initialisation(n: int):
     return [(i, 0) for i in range(n + 1)]
 
 
+def maj_chemin(chemin: Chemin, pivots_utilisables: list[int]) -> tuple[Chemin, list[int]]:
+    while True:
+        angles_exclus = []
+        i_piv = pivots_utilisables[randrange(len(pivots_utilisables))]
+
+        a = genere_a(chemin, i_piv, angles_exclus)
+        nv_chemin = rotation(chemin, i_piv, a)
+
+        if est_CAE(nv_chemin):
+            return nv_chemin, pivots_utilisables
+
+        angles_exclus.append(a)
+
+        # Cas ou le chemin se replie sur lui-meme, on change de pivot
+        if len(angles_exclus) == 3:
+            pivots_utilisables.remove(i_piv)
+            i_piv = pivots_utilisables[randrange(len(pivots_utilisables))]
+
+
 def genere_chemin_pivot(n: int, n_rot: int) -> Chemin:
     """
     Genere un chemin en utilisant la methode du pivot
     """
     chemin = initialisation(n)
+    pivots_utilisables = list(range(1, n))
 
     for _ in range(n_rot):
+        nv_chemin, pivots_utilisables = maj_chemin(chemin, pivots_utilisables)
 
-        # Utiliser le dernier point comme pivot ne change rien
-        i_piv = randrange(n - 1)
-
-        # A quand une boucle do while ...
-        a = genere_a(chemin, i_piv)
-        nv_chemin = rotation(chemin, i_piv, a)
-
-        while not est_CAE(nv_chemin):
-            a = genere_a(chemin, i_piv)
-            nv_chemin = rotation(nv_chemin, i_piv, a)
+        assert nv_chemin != chemin
 
         chemin = nv_chemin
+
 
     return chemin
 
@@ -234,8 +277,11 @@ fig, ax = plt.subplots()
 X = [] 
 Y = []
 
-chemin = genere_chemin_pivot(100, 15)
+for _ in range(100):
+    chemin = genere_chemin_pivot(100, 10)
+    print("ok")
 
+exit()
 for x, y in chemin:
     X.append(x)
     Y.append(y)
